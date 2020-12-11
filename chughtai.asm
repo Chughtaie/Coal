c12086da3c026594699c60344150d9ec996c5209
Chughtae <Chughtae@github.com>
2020-12-11 18:50:36 +0500

    Update chughtai.asm

--- chughtai.asm
+++ chughtai.asm
@@ -10,28 +10,74 @@ mov al,c
 mov marior1 , al
 mov al,d
 mov marior2, al
+;22,23,4,5
 
 
-
-	drawbox 10111101b,marioc1,marioc2,marior1,marior2
+	drawbox 10111101b,marioc1,marioc2,marior1,marior2	;middle body
 dec marioc1	
 sub marioc2,2	
 dec marior1	
 inc marior2
 	
-	drawbox 11111101b,marioc1,marioc2,marior1,marior2
+	drawbox 11111101b,marioc1,marioc2,marior1,marior2	;head
 add marioc1,3
 add marioc2,3
 sub marior2,3	
 	
-	drawbox 11111101b,marioc1,marioc2,marior1,marior2
+	drawbox 11111101b,marioc1,marioc2,marior1,marior2	;left leg
 add marior1,3
 add marior2,3	
 	
-	drawbox 11111101b,marioc1,marioc2,marior1,marior2
-	;ret
-endm
+	drawbox 11111101b,marioc1,marioc2,marior1,marior2	;right leg
 
+endm
+;----------------------------------------------------------------------------
+drawenemy macro a,b
+mov al,a
+mov bl,b
+mov en1r1,al
+mov en1r2,bl
+mov en1c1,22
+mov en1c2,23
+
+
+
+	drawbox 01001101b,en1c1,en1c2,en1r1,en1r2
+dec en1c1	
+sub en1c2,2	
+dec en1r1	
+inc en1r2
+	
+	drawbox 11011101b,en1c1,en1c2,en1r1,en1r2
+add en1c1,3
+add en1c2,3
+sub en1r2,3	
+	
+	drawbox 11011101b,en1c1,en1c2,en1r1,en1r2	;left leg
+add en1r1,3
+add en1r2,3	
+	
+	drawbox 11011101b,en1c1,en1c2,en1r1,en1r2	;right leg
+
+
+
+endm
+;------------------------------------------------------------------------------
+
+drawbigenemy macro a,b,c,d
+mov al,a
+mov en1c1 , al
+mov al,b
+mov en1c2 , al
+mov al,c
+mov en1r1 , al
+mov al,d
+mov en1r2, al
+
+	drawbox 01001101b,en1c1,en1c2,en1r1,en1r2
+
+
+endm
 
 
 displaycharacter macro char
@@ -93,14 +139,35 @@ endm
 	chj db '*$'
 	count db 0
 	var db 0
+en1r1 db 0
+en1r2 db 0	
+
+er1 db 21
+er2 db 22
+en1c1 db ?
+en1c2 db ?
+counte1 db 0
+counte2 db 0
+	msg db "CONGRATS!!! YOU WON$"
+	msglost db "YOU LOST!!!LOSER...$"
+	enterr db "ENTER YOUR NAME : $"
+	welcome db "WELCOME TO SUPER MARIO$"
+	press db "Press any key to contine...$"
+bc1 db 1
+bc2 db 2
+br1 db 65	
+br2 db 69
 oc1 db 22
 oc2 db 23
 or1 db 4
 or2 db 5
+subb db ?
+namee db 30 dup("$")
 marioc1 db 0
 marioc2 db 0
 marior1 db 0
 marior2 db 0
+lvl db 1
 
 
 
@@ -114,7 +181,7 @@ Column_poistion db 0
 
   mov ax,@data
   mov ds,ax
-  
+  call input
 
 	
 	
@@ -124,10 +191,7 @@ Column_poistion db 0
 call clearscreen
 
 drawbox 10111101b,25,26,4,8
- ;drawm c1,c2,r1,r2
-	;add r1,5
-	;add r2,5
-;mov cx,2
+
 	call clearscreen
 	call displayl1
 	drawm oc1,oc2,or1,or2
@@ -138,11 +202,6 @@ drawbox 10111101b,25,26,4,8
 l1:
 
 	
-	
-;call drawline
-	
-	;call displayl1
-	
 	mov ah,1
 	int 16h
 	
@@ -152,13 +211,9 @@ l1:
 	call move
 	.if ah==-1
 		jmp exit
+	.elseif or2 >75
+		call main2
 	.endif
-	;call clearscreen
-	;drawm c1,c2,r1,r2
-	;inc c1
-	;inc c2
-	;add r1,5
-	;add r2,
 	jmp l1
 	
 exit:	
@@ -174,39 +229,235 @@ INT 21h
 
 main endp
 
+input proc
+
+mov ah,9
+mov dx,offset enterr
+int 21h
+
+
+
+
+mov ah,3fh
+mov dx,offset namee
+int 21h
+
+
+
+mov ah,06
+mov al,0
+;mov cx,0
+mov ch,0	;c1
+mov cl,00	;r1
+mov dh,30	;c1
+mov dl,79	;r2
+
+mov bh,10011101b
+int 10h
+mov ah,02
+mov bh,0
+mov dh,12
+mov dl,30
+int 10h
+lea dx,namee
+mov ah,09
+int 21h
+
+mov bh,10011101b
+int 10h
+mov ah,02
+mov bh,0
+mov dh,10
+mov dl,24
+int 10h
+
+lea dx,welcome
+mov ah,09
+int 21h
+
+mov bh,10011101b
+int 10h
+mov ah,02
+mov bh,0
+mov dh,14
+mov dl,24
+int 10h
+
+lea dx,press
+mov ah,09
+int 21h
+
+
+mov dh,12
+mov dl,30
+int 10h 
+
+mov ah,1
+int 21h
+
+
+input endp
+
+ret
+main2 proc
+  mov ax,0
+  mov ax,0
+  mov lvl,2
+; .while ah!=1
+call clearscreen
+
+drawbox 10111101b,25,26,4,8
+
+mov oc1, 22
+mov oc2,23
+mov or1,4
+mov or2,5
+	call clearscreen
+	call display2
+	drawm oc1,oc2,or1,or2
+	
+	
+
+	
+l2:
+
+	
+	mov ah,1
+	int 16h
+	
+	; jz l1
+	mov ah,0
+	int 16h
+	call move
+	.if ah==-1
+		jmp exit2	
+	.elseif or2 >75
+		call main3
+	.endif
+
+	jmp l2
+	
+exit2:	
+LOL2:
+; .endw	
+	;Gotoxy 0,0
+	;character
+
+
+
+MOV AH,4CH
+INT 21h
+ret
+main2 endp
+
+
+
+main3 proc
+
+  mov ax,0
+  mov ax,0
+  mov lvl,3
+; .while ah!=1
+call clearscreen
+
+drawbox 10111101b,25,26,4,8
+
+mov oc1, 22
+mov oc2,23
+mov or1,4
+mov or2,5
+	call clearscreen
+	call display3
+	drawm oc1,oc2,or1,or2
+	
+	
+
+	
+l3:
+
+	
+	mov ah,1
+	int 16h
+
+	mov ah,0
+	int 16h
+	call move
+	
+	.if ah==-1
+		jmp exit3
+	.elseif or2 >75
+		call wingame
+	.endif
+	jmp l3
+	
+exit3:	
+LOL3:
+
+MOV AH,4CH
+INT 21h
+ret
+
+
+
+
+main3 endp
+
+
+
+
+
+
+
+
+
+enemymov proc
+
+
+.if counte1<=10			
+		
+			add er1,1
+			add er2,1
+			inc counte1
+			;call clearscreen
+;drawbox 11110101b,23,24,er1,er2
+drawenemy er1,er2		
+		
+ .elseif  counte2<=10
+			.if counte2==10
+			mov counte1,1
+			mov counte2,0
+			.endif
+			;call delay2
+			sub er1,1
+			sub er2,1
+		inc counte2
+			;drawbox 11110101b,23,24,er1,er2
+		drawenemy er1,er2		
+
+		
+	.endif
+
+ret
+enemymov endp
+
+
+
+
+
 
 Move proc
 	
 	.if ah==04Dh || ah==20h;right,D
-	call clearscreen
-	call displayl1
-	add or1,2
-	add or2,2
-	;inc marioc1
-	;inc marioc2
-	;inc marior1
-	;inc marior2
-	drawm oc1,oc2,or1,or2
-	;inc c1
-	;inc c2
+	
+		call rightmov
 	
 	.elseif ah==4Bh || ah==1Eh	;left,A
 	
-	call clearscreen
-	call displayl1
-	sub or1,2
-	sub or2,2
-	;inc marioc1
-	;inc marioc2
-	;inc marior1
-	;inc marior2
-	drawm oc1,oc2,or1,or2
-
-	
+		call leftmov
 	
 	.elseif ah==48h || ah==11h	;Up,W	
 
-	call up
+		call up
 	
 	.elseif ah==50h || ah==1Fh	;Down,S	
 	
@@ -220,42 +471,241 @@ Move proc
 	ret
 move endp
 
+endgame proc
+;call clearscreen
+
+
+mov ah,06
+mov al,0
+;mov cx,0
+mov ch,0	;c1
+mov cl,00	;r1
+mov dh,30	;c1
+mov dl,79	;r2
+
+mov bh,10001101b
+int 10h
+mov ah,02
+mov bh,0
+mov dh,12
+mov dl,30
+int 10h
+lea dx,msglost
+mov ah,09
+int 21h
+
+
+mov dh,12
+mov dl,30
+int 10h 
+
+mov ah,4ch
+int 21h
+
+
+ret
+
+endgame endp
+
+rightmov proc
+
+.if   or1==14 || or1==30 || or1==48 || or1==62 
+mov ax,ax
+.else
+
+	add or1,2
+	add or2,2
+	
+	call show
+
+.endif
+call collision
+call detectup
+ret
+rightmov endp
+
+
+
+
+
+leftmov proc
+;.if  oc2 > 22 && or1>5 && or1<15 || or1>23 && or1 < 33 || or1>41 && or1<50 || or1> 57 && or1<66 || or1>70
+.if  (or1==22 || or1==38 || or1==56 || or1==70)
+mov ax,ax
+.elseif or1>4
+
+
+	sub or1,2
+	sub or2,2
+	
+	call show
+	
+.endif
+call detectup
+call collision
+ret
+leftmov endp
+
+
+
 up proc
 
 	;call clearscreen
 	;call displayl1
+.if oc1==14
+	ret
+.endif
+mov cx,2
 
-mov cx,6
-sub oc1,8
-sub oc2,8
-mov var,6
+mov oc1,14
+mov oc2,15
+
+call show
+mov var,1
+
 	jumpp:
-		call delay
-		call clearscreen
-		call displayl1
+	
+		;call delay
+		
 		add or1,2
 		add or2,2	
-		drawm oc1,oc2,or1,or2
+		call show
+		
+		call delay
 		dec var
+
 	.if var>0
-	jmp jumpp
+		jmp jumpp
 	.endif
 	
-call clearscreen
-call displayl1
-add oc1,8
-add oc2,8
-;add or1,2
-;add or2,2	
-drawm oc1,oc2,or1,or2
-		
+	
+call detectup
+
+call show
+	
 ret 
 up endp
 
+;====================================================================================
+detectup proc
+
+.if (or1 > 14 && or1 <= 20) || (or1 > 30 && or1<=38) || (or1 > 48 && or1<=54) || (or1 > 62 && or1<=68)
+	mov ax,ax
+	mov oc1,14
+	mov oc2,15
+	call show
+	ret
+.else
+
+mov oc1,22
+mov oc2,23
+call show
+
+.endif
+
+eeend:
+
+ret
+detectup endp
+
+
+Show proc
+
+	call clearscreen
+	.if lvl==1
+	call displayl1
+	.elseif lvl==2
+	call display2
+	.elseif lvl==3
+	call display3
+	.endif
+
+	drawm  oc1,oc2,or1,or2
+	
+	.if lvl==2 || lvl==3
+		call enemymov
+	.endif
+	ret
+
+SHOW endp
+
+
+collision proc
+
+.if lvl==2 || lvl==3
+	mov bl,er1
+mov al,er2
+ 
+	.if bl == or1 || al==or1
+		call endgame
+	
+	.endif
+	sub bl,1
+	sub al,1
+	.if bl == or1 || al==or1
+		call endgame
+	
+	.endif
+	add bl,2
+	add al,2
+	.if bl == or1 || al==or1
+		call endgame
+	
+	.endif
+	add bl,1
+	add bl,1
+	.if bl == or1 || al==or1
+		call endgame
+	
+	.endif
+	sub bl,4
+	sub al,4
+	.if bl == or1  || al==or1
+		call endgame
+	.endif
+.endif
+
+ret
+collision endp
+
+wingame proc
+
+mov ah,06
+mov al,0
+;mov cx,0
+mov ch,0	;c1
+mov cl,00	;r1
+mov dh,30	;c1
+mov dl,79	;r2
+
+mov bh,10001101b
+int 10h
+mov ah,02
+mov bh,0
+mov dh,12
+mov dl,30
+int 10h
+lea dx,msg
+mov ah,09
+int 21h
+
+
+mov dh,12
+mov dl,30
+int 10h 
+
+ret
+
+
+wingame endp
+
+
+
+
 displayl1 proc
 	;flag
-	drawbox 11111111b,0,24,78,78
-	drawbox 10101111b,0,3,66,77
+	drawbox 11111111b,2,24,78,78
+	drawbox 10101111b,2,5,66,77
  
  
 		;obstacles
@@ -275,6 +725,76 @@ displayl1 proc
 	;---------------------------------
 
 displayl1 endp
+
+
+
+display2 proc
+;flag
+	drawbox 01111111b,2,24,78,78
+	drawbox 01011111b,2,5,66,77
+ 
+ 
+		;obstacles
+	drawbox 10011111b,19,24,18,19
+	drawbox 00111111b,18,19,17,20
+
+	drawbox 10011111b,18,24,35,36
+	drawbox 00111111b,17,18,34,37
+
+
+
+	drawbox 10011111b,19,24,52,53
+	drawbox 00111111b,18,19,51,54
+
+
+	drawbox 10011111b,23,24,65,67
+	
+	ret
+	;---------------------------------
+	
+display2 endp
+
+
+
+display3 proc
+;flag
+	drawbox 11111111b,15,21,76,76
+	drawbox 10101111b,15,16,71,75
+	
+	drawbox 11001111b,18,24,78,80
+	drawbox 01111111b,20,24,75,77
+	drawbox 11001111b,18,24,72,74
+  
+		;obstacles
+	drawbox 10011111b,19,24,18,19
+	drawbox 00111111b,18,19,17,20
+
+	drawbox 10011111b,18,24,35,36
+	drawbox 00111111b,17,18,34,37
+
+
+
+	drawbox 10011111b,19,24,52,53
+	drawbox 00111111b,18,19,51,54
+
+
+	drawbox 10011111b,23,24,65,67
+	drawbigenemy bc1,bc2,br1,br2
+	ret
+
+
+
+
+
+display3 endp
+
+
+
+
+
+
+
+
 
 comment&
 drawmario proc
@@ -338,7 +858,7 @@ push dx
 
 mov cx,1000
 mydelay:
-mov bx,873      ;; increase this number if you want to add more delay, and decrease this number if you want to reduce delay.
+mov bx,800      ;; increase this number if you want to add more delay, and decrease this number if you want to reduce delay.
 mydelay1:
 dec bx
 jnz mydelay1
@@ -353,6 +873,34 @@ pop ax
 ret
 
 delay endp
+
+
+delay2 proc
+
+
+push ax
+push bx
+push cx
+push dx
+
+mov cx,1000
+mydelay:
+mov bx,200      ;; increase this number if you want to add more delay, and decrease this number if you want to reduce delay.
+mydelay1:
+dec bx
+jnz mydelay1
+loop mydelay
+
+
+pop dx
+pop cx
+pop bx
+pop ax
+
+ret
+
+delay2 endp
+
 
 
 
@@ -372,7 +920,7 @@ box proc
 	ret
 box endp
 
-	clearscreen proc
+clearscreen proc
 	mov al,03
 	mov ah,0
 	int 10h
@@ -382,6 +930,36 @@ clearscreen endp
 
 
 
+
+
+pushaa proc
+push dx
+	push cx
+	push bx
+	push ax
+	ret
+pushaa endp
+
+popaa proc
+pop ax
+	pop bx
+	pop cx
+	pop dx
+	ret
+popaa endp 
+
+
+
+
+
+
+clearreg proc
+	xor ax,ax
+	xor bx,bx
+	xor cx,cx
+	xor dx,dx
+	ret
+clearreg endp
 draw_border proc
 
 	call clearscreen
@@ -496,34 +1074,6 @@ ret
 draw_border endp
 
 
-pushaa proc
-push dx
-	push cx
-	push bx
-	push ax
-	ret
-pushaa endp
-
-popaa proc
-pop ax
-	pop bx
-	pop cx
-	pop dx
-	ret
-popaa endp
-
-
-
-
-
-
-clearreg proc
-	xor ax,ax
-	xor bx,bx
-	xor cx,cx
-	xor dx,dx
-	ret
-clearreg endp
 
 
 endline proc
